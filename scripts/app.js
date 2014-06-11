b989c325eba404168f0fd947148f5a06db65bde3
Chris Mills <cmills@opera.com>
2014-06-11 12:26:21 +0100

    minor fix

--- scripts/app.js
+++ scripts/app.js
@@ -50,8 +50,7 @@ if (navigator.getUserMedia) {
          analyser.connect(distortion);
          distortion.connect(gainNode);
          gainNode.connect(audioCtx.destination);
-         gainNode.gain.value = 0;
-
+        
       	 visualize(stream);
          voiceChange();
 
