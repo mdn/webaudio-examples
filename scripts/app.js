a2f6323ccf6477c5d72da3e070f305f471350d47
Chris Mills <cmills@opera.com>
2014-06-12 13:35:00 +0100

    doing some linting

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,15 @@
 // fork getUserMedia for multiple browser versions, for those
 // that need prefixes
 
-navigator.getUserMedia = ( navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
+navigator.getUserMedia = (navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia);
 
 // set up forked web audio context, for multiple browsers
 // window. is needed otherwise Safari explodes
 
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();;
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var voiceSelect = document.getElementById("voice");
 var source;
 var stream;
