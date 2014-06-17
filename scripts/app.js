8f4c993e1ef903252331a31dc434f1e7f7a691c0
Chris Mills <cmills@opera.com>
2014-06-17 07:40:35 -0700

    added rudimentary keyboard controls

--- scripts/app.js
+++ scripts/app.js
@@ -35,11 +35,13 @@ var CurX;
 var CurY;
 
 // Get new mouse pointer coordinates when mouse is moved
-// then set new gain and putch values
+// then set new gain and pitch values
 
 document.onmousemove = updatePage;
 
-function updatePage(e) {   
+function updatePage(e) {
+    KeyFlag = false;
+
     CurX = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
     CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
     
@@ -81,8 +83,13 @@ canvas.height = HEIGHT;
 var canvasCtx = canvas.getContext('2d');
 
 function canvasDraw() {
-  rX = CurX;
-  rY = CurY;
+  if(KeyFlag == true) {
+    rX = KeyX;
+    rY = KeyY;
+  } else {
+    rX = CurX;
+    rY = CurY;
+  }
   rC = Math.floor((gainNode.gain.value/maxVol)*30);
   
   canvasCtx.globalAlpha = 0.2;
@@ -102,4 +109,58 @@ var clear = document.querySelector('.clear');
 
 clear.onclick = function() {
   canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+// keyboard controls
+
+var body = document.querySelector('body');
+
+var KeyX = 1;
+var KeyY = 0.01;
+var KeyFlag = false;
+
+body.onkeydown = function(e) {
+  KeyFlag = true;
+
+  // 37 is arrow left, 39 is arrow right,
+  // 38 is arrow up, 40 is arrow down
+
+  if(e.keyCode == 37) {
+    KeyX -= 20;
+  };
+
+  if(e.keyCode == 39) {
+    KeyX += 20;
+  };
+
+  if(e.keyCode == 38) {
+    KeyY -= 20;
+  };
+
+  if(e.keyCode == 40) {
+    KeyY += 20;
+  };
+
+  // set max and min constraints for KeyX and KeyY
+
+  if(KeyX < 1) {
+    KeyX = 1;
+  };
+
+  if(KeyX > WIDTH) {
+    KeyX = WIDTH;
+  };
+
+  if(KeyY < 0.01) {
+    KeyY = 0.01;
+  };
+
+  if(KeyY > HEIGHT) {
+    KeyY = HEIGHT;
+  };
+
+  oscillator.frequency.value = (KeyX/WIDTH) * maxFreq;
+  gainNode.gain.value = (KeyY/HEIGHT) * maxVol;
+
+  canvasDraw();
 }
