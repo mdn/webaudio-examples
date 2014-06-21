ed1165236c8c3990f01606c08d9517f322571eb0
Chris Mills <cmills@opera.com>
2014-06-21 05:46:52 +0100

    adding frequency visualisation

--- scripts/app.js
+++ scripts/app.js
@@ -74,6 +74,10 @@ ajaxRequest.send();
 var canvas = document.querySelector('.visualizer');
 var canvasCtx = canvas.getContext("2d");
 
+var intendedWidth = document.querySelector('.wrapper').clientWidth;
+
+canvas.setAttribute('width',intendedWidth);
+
 var visualSelect = document.getElementById("visual");
 
 var drawVisual;
@@ -160,6 +164,38 @@ function visualize(stream) {
 
       canvasCtx.lineTo(canvas.width, canvas.height/2);
       canvasCtx.stroke();
+    };
+
+    draw();
+
+  } else if(visualSetting == "frequencybars") {
+    analyser.fftSize = 256;
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+    var dataArray = new Uint8Array(bufferLength);
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    function draw() {
+      drawVisual = requestAnimationFrame(draw);
+
+      analyser.getByteFrequencyData(dataArray);
+
+      canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      var barWidth = (WIDTH / bufferLength) * 2.5;
+      var barHeight;
+      var x = 0;
+
+      for(var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+
+        canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+        canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
+
+        x += barWidth + 1;
+      }
     };
 
     draw();
