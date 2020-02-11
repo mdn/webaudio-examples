b2c121bfa5f7c77384face662f1d9daaab8a729d
Chris Mills <cmills@mozilla.com>
2020-02-11 16:51:45 +0000

    updating examples to use let and const, and have the audio contexts be created in reaction to a user gesture where possible

--- panner-node/main.js
+++ panner-node/main.js
@@ -1,150 +1,162 @@
 // set up listener and panner position information
-var WIDTH = window.innerWidth;
-var HEIGHT = window.innerHeight;
+let WIDTH = window.innerWidth;
+let HEIGHT = window.innerHeight;
 
-var xPos = Math.floor(WIDTH/2);
-var yPos = Math.floor(HEIGHT/2);
-var zPos = 295;
+let xPos = Math.floor(WIDTH/2);
+let yPos = Math.floor(HEIGHT/2);
+let zPos = 295;
 
-// define other variables
+// play, stop, and other important dom nodes
 
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioCtx = new AudioContext();
+const play = document.querySelector('.play');
+const stop = document.querySelector('.stop');
+const boomBox = document.querySelector('.boom-box');
+const listenerData = document.querySelector('.listener-data');
+const pannerData = document.querySelector('.panner-data');
+const pulseWrapper = document.querySelector('.pulse-wrapper');
 
-var panner = audioCtx.createPanner();
-panner.panningModel = 'HRTF';
-panner.distanceModel = 'inverse';
-panner.refDistance = 1;
-panner.maxDistance = 10000;
-panner.rolloffFactor = 1;
-panner.coneInnerAngle = 360;
-panner.coneOuterAngle = 0;
-panner.coneOuterGain = 0;
+//  movement controls and initial data
 
-if(panner.orientationX) {
-  panner.orientationX.value = 1;
-  panner.orientationY.value = 0;
-  panner.orientationZ.value = 0;
-} else {
-  panner.setOrientation(1,0,0);
-}
+const leftButton = document.querySelector('.left');
+const rightButton = document.querySelector('.right');
+const zoomInButton = document.querySelector('.zoom-in');
+const zoomOutButton = document.querySelector('.zoom-out');
 
-var listener = audioCtx.listener;
+let boomX = 0;
+let boomY = 0;
+let boomZoom = 0.50;
 
-if(listener.forwardX) {
-  listener.forwardX.value = 0;
-  listener.forwardY.value = 0;
-  listener.forwardZ.value = -1;
-  listener.upX.value = 0;
-  listener.upY.value = 1;
-  listener.upZ.value = 0;
-} else {
-  listener.setOrientation(0,0,-1,0,1,0);
-}
+// variables to hold information that needs to be assigned upon play
 
-var source;
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+let audioCtx;
+let panner;
+let listener;
+let source;
 
-var play = document.querySelector('.play');
-var stop = document.querySelector('.stop');
+function init() {
 
-var boomBox = document.querySelector('.boom-box');
+  audioCtx = new AudioContext();
+  panner = audioCtx.createPanner();
+  listener = audioCtx.listener;
 
-var listenerData = document.querySelector('.listener-data');
-var pannerData = document.querySelector('.panner-data');
+  panner.panningModel = 'HRTF';
+  panner.distanceModel = 'inverse';
+  panner.refDistance = 1;
+  panner.maxDistance = 10000;
+  panner.rolloffFactor = 1;
+  panner.coneInnerAngle = 360;
+  panner.coneOuterAngle = 0;
+  panner.coneOuterGain = 0;
 
-leftBound = (-xPos) + 50;
-rightBound = xPos - 50;
-
-xIterator = WIDTH/150;
-
-// listener will always be in the same place for this demo
-
-if(listener.positionX) {
-  listener.positionX.value = xPos;
-  listener.positionY.value = yPos;
-  listener.positionZ.value = 300;
-} else {
-  listener.setPosition(xPos,yPos,300);
-}
-
-listenerData.innerHTML = 'Listener data: X ' + xPos + ' Y ' + yPos + ' Z ' + 300;
-
-// panner will move as the boombox graphic moves around on the screen
-function positionPanner() {
-  if(panner.positionX) {
-    panner.positionX.value = xPos;
-    panner.positionY.value = yPos;
-    panner.positionZ.value = zPos;
+  if(panner.orientationX) {
+    panner.orientationX.value = 1;
+    panner.orientationY.value = 0;
+    panner.orientationZ.value = 0;
   } else {
-    panner.setPosition(xPos,yPos,zPos);
+    panner.setOrientation(1,0,0);
   }
-  pannerData.innerHTML = 'Panner data: X ' + xPos + ' Y ' + yPos + ' Z ' + zPos;
+
+  if(listener.forwardX) {
+    listener.forwardX.value = 0;
+    listener.forwardY.value = 0;
+    listener.forwardZ.value = -1;
+    listener.upX.value = 0;
+    listener.upY.value = 1;
+    listener.upZ.value = 0;
+  } else {
+    listener.setOrientation(0,0,-1,0,1,0);
+  }
+
+  leftBound = (-xPos) + 50;
+  rightBound = xPos - 50;
+
+  xIterator = WIDTH/150;
+
+  // listener will always be in the same place for this demo
+
+  if(listener.positionX) {
+    listener.positionX.value = xPos;
+    listener.positionY.value = yPos;
+    listener.positionZ.value = 300;
+  } else {
+    listener.setPosition(xPos,yPos,300);
+  }
+
+  listenerData.innerHTML = 'Listener data: X ' + xPos + ' Y ' + yPos + ' Z ' + 300;
+
+  // panner will move as the boombox graphic moves around on the screen
+  function positionPanner() {
+    if(panner.positionX) {
+      panner.positionX.value = xPos;
+      panner.positionY.value = yPos;
+      panner.positionZ.value = zPos;
+    } else {
+      panner.setPosition(xPos,yPos,zPos);
+    }
+    pannerData.innerHTML = 'Panner data: X ' + xPos + ' Y ' + yPos + ' Z ' + zPos;
+  }
+
+
+  // use XHR to load an audio track, and
+  // decodeAudioData to decode it and stick it in a buffer.
+  // Then we put the buffer into the source
+
+  function getData() {
+    source = audioCtx.createBufferSource();
+    request = new XMLHttpRequest();
+
+    request.open('GET', 'viper.ogg', true);
+
+    request.responseType = 'arraybuffer';
+
+
+    request.onload = function() {
+      let audioData = request.response;
+
+      audioCtx.decodeAudioData(audioData, function(buffer) {
+          myBuffer = buffer;
+          source.buffer = myBuffer;
+
+          source.connect(panner);
+          panner.connect(audioCtx.destination);
+          positionPanner();
+          source.loop = true;
+        },
+
+        function(e){
+          console.log("Error with decoding audio data" + e.err);
+        });
+
+    };
+
+    request.send();
+  }
+
+  getData();
 }
-
-
-// use XHR to load an audio track, and
-// decodeAudioData to decode it and stick it in a buffer.
-// Then we put the buffer into the source
-
-function getData() {
-  source = audioCtx.createBufferSource();
-  request = new XMLHttpRequest();
-
-  request.open('GET', 'viper.ogg', true);
-
-  request.responseType = 'arraybuffer';
-
-
-  request.onload = function() {
-    var audioData = request.response;
-
-    audioCtx.decodeAudioData(audioData, function(buffer) {
-        myBuffer = buffer;
-        source.buffer = myBuffer;
-
-        source.connect(panner);
-        panner.connect(audioCtx.destination);
-        positionPanner();
-        source.loop = true;
-      },
-
-      function(e){
-        console.log("Error with decoding audio data" + e.err);
-      });
-
-  };
-
-  request.send();
-}
-
 // wire up buttons to stop and play audio
 
-var pulseWrapper = document.querySelector('.pulse-wrapper');
+stop.setAttribute('disabled', 'disabled');
 
 play.onclick = function() {
-  getData();
+  init();
   source.start(0);
+
   play.setAttribute('disabled', 'disabled');
+  stop.removeAttribute('disabled');
   pulseWrapper.classList.add('pulsate');
 };
 
 stop.onclick = function() {
   source.stop(0);
+  stop.setAttribute('disabled', 'disabled');
   play.removeAttribute('disabled');
   pulseWrapper.classList.remove('pulsate');
 };
 
 // controls to move left and right past the boom box
 // and zoom in and out
-
-var leftButton = document.querySelector('.left');
-var rightButton = document.querySelector('.right');
-var zoomInButton = document.querySelector('.zoom-in');
-var zoomOutButton = document.querySelector('.zoom-out');
-
-var boomX = 0;
-var boomY = 0;
-var boomZoom = 0.50;
 
 function moveRight() {
   boomX += -xIterator;
