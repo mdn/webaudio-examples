abaf0244abeb289b48a440dfd0fb10a1d05647fb
Jean-Yves Perrier <jypenator@gmail.com>
2022-07-18 09:49:32 +0200

    Update panner-mode to modern JS

--- panner-node/main.js
+++ panner-node/main.js
@@ -8,8 +8,8 @@ let zPos = 295;
 
 // play, stop, and other important dom nodes
 
-const play = document.querySelector('.play');
-const stop = document.querySelector('.stop');
+const playBtn = document.querySelector('.play');
+const stopBtn = document.querySelector('.stop');
 const boomBox = document.querySelector('.boom-box');
 const listenerData = document.querySelector('.listener-data');
 const pannerData = document.querySelector('.panner-data');
@@ -26,45 +26,43 @@ let boomX = 0;
 let boomY = 0;
 let boomZoom = 0.50;
 
-// variables to hold information that needs to be assigned upon play
-
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-let audioCtx;
+// Variables to hold information that needs to be assigned upon play
+// Audio context must be created only after the user has interacted.
+let audioCtx; 
 let panner;
 let listener;
 let source;
 
 function init() {
   audioCtx = new AudioContext();
-  panner = audioCtx.createPanner();
   listener = audioCtx.listener;
+  console.log(listener);
 
-  panner.panningModel = 'HRTF';
-  panner.distanceModel = 'inverse';
-  panner.refDistance = 1;
-  panner.maxDistance = 10000;
-  panner.rolloffFactor = 1;
-  panner.coneInnerAngle = 360;
-  panner.coneOuterAngle = 0;
-  panner.coneOuterGain = 0;
+  panner = new PannerNode(audioCtx, {
+    panningModel: 'HRTF',
+    distanceModel: 'inverse',
+    refDistance: 1,
+    maxDistance: 10000,
+    rolloffFactor: 1,
+    coneInnerAngle: 360,
+    coneOuterAngle: 0,
+    coneOuterGain: 0,
+    orientationX: 1,
+    orientationY: 0,
+    orientationZ: 0
+  });
 
-  if(panner.orientationX) {
-    panner.orientationX.value = 1;
-    panner.orientationY.value = 0;
-    panner.orientationZ.value = 0;
+  if (!listener.forwardX) {
+    // Deprecated but still needed (July 2022)
+    listener.setOrientation(0, 0, -1, 0, 1, 0);
   } else {
-    panner.setOrientation(1,0,0);
-  }
-
-  if(listener.forwardX) {
+    // Standard way
     listener.forwardX.value = 0;
     listener.forwardY.value = 0;
     listener.forwardZ.value = -1;
     listener.upX.value = 0;
     listener.upY.value = 1;
     listener.upZ.value = 0;
-  } else {
-    listener.setOrientation(0,0,-1,0,1,0);
   }
 
   leftBound = (-xPos) + 50;
@@ -74,69 +72,53 @@ function init() {
 
   // listener will always be in the same place for this demo
 
-  if(listener.positionX) {
+  if (!listener.positionX) {
+    // Deprecated but still needed (July 2022)
+    listener.setPosition(xPos, yPos, 300);
+  } else {
+    // Standard way
     listener.positionX.value = xPos;
     listener.positionY.value = yPos;
     listener.positionZ.value = 300;
-  } else {
-    listener.setPosition(xPos,yPos,300);
   }
 
-  listenerData.innerHTML = 'Listener data: X ' + xPos + ' Y ' + yPos + ' Z ' + 300;
+  listenerData.textContent = `Listener data: X ${xPos} Y ${yPos} Z ${300}`;
 
   // panner will move as the boombox graphic moves around on the screen
   function positionPanner() {
-    if(panner.positionX) {
-      panner.positionX.value = xPos;
-      panner.positionY.value = yPos;
-      panner.positionZ.value = zPos;
-    } else {
-      panner.setPosition(xPos,yPos,zPos);
-    }
-    pannerData.innerHTML = 'Panner data: X ' + xPos + ' Y ' + yPos + ' Z ' + zPos;
+    panner.positionX.value = xPos;
+    panner.positionY.value = yPos;
+    panner.positionZ.value = zPos;
+    pannerData.textContent = `Panner data: X ${xPos} Y ${yPos} Z ${300}`;
   }
 
-
-  // use XHR to load an audio track, and
-  // decodeAudioData to decode it and stick it in a buffer.
+  // Fetch an audio track, decode it and stick it in a buffer.
   // Then we put the buffer into the source
-
   function getData() {
-    source = audioCtx.createBufferSource();
-    request = new XMLHttpRequest();
-
-    request.open('GET', 'viper.ogg', true);
-
-    request.responseType = 'arraybuffer';
-
-
-    request.onload = function() {
-      let audioData = request.response;
-
-      audioCtx.decodeAudioData(audioData, function(buffer) {
-          myBuffer = buffer;
-          source.buffer = myBuffer;
-
-          source.connect(panner);
-          panner.connect(audioCtx.destination);
-          positionPanner();
-          source.loop = true;
-        },
-
-        function(e){
-          console.log("Error with decoding audio data" + e.err);
+    fetch("viper.ogg")
+      .then((response) => response.arrayBuffer())
+      .then((downloadedBuffer) => audioCtx.decodeAudioData(downloadedBuffer))
+      .then((decodedBuffer) => {
+        source = new AudioBufferSourceNode(audioCtx, {
+          buffer: decodedBuffer
         });
-
-    };
-
-    request.send();
+        source.connect(panner);
+        panner.connect(audioCtx.destination);
+        positionPanner();
+        source.loop = true;
+        console.log("Loaded!");
+        source.start(0);
+      })
+      .catch((e) => {
+        console.error(`Error while preparing the audio data ${e.err}`);
+      }
+    );
   }
 
   getData();
 
   // controls to move left and right past the boom box
   // and zoom in and out
-
   function moveRight() {
     boomX += -xIterator;
     xPos += -0.066;
@@ -146,8 +128,7 @@ function init() {
       xPos = (WIDTH/2) - 5;
     }
 
-    boomBox.style.webkitTransform = "translate(" + boomX + "px , " + boomY + "px) scale(" + boomZoom + ")";
-    boomBox.style.transform = "translate(" + boomX + "px , " + boomY + "px) scale(" + boomZoom + ")";
+    boomBox.style.transform = `translate(${boomX}px, ${boomY}px) scale(${boomZoom})`;
     positionPanner();
     rightLoop = requestAnimationFrame(moveRight);
     return rightLoop;
@@ -163,8 +144,7 @@ function init() {
     }
 
     positionPanner();
-    boomBox.style.webkitTransform = "translate(" + boomX + "px , " + boomY + "px) scale(" + boomZoom + ")";
-    boomBox.style.transform = "translate(" + boomX + "px , " + boomY + "px) scale(" + boomZoom + ")";
+    boomBox.style.transform = `translate(${boomX}px, ${boomY}px) scale(${boomZoom})`;
     leftLoop = requestAnimationFrame(moveLeft);
     return leftLoop;
   }
@@ -179,8 +159,7 @@ function init() {
     }
 
     positionPanner();
-    boomBox.style.webkitTransform = "translate(" + boomX + "px , " + boomY + "px) scale(" + boomZoom + ")";
-    boomBox.style.transform = "translate(" + boomX + "px , " + boomY + "px) scale(" + boomZoom + ")";
+    boomBox.style.transform = `translate(${boomX}px, ${boomY}px) scale(${boomZoom})`;
     zoomInLoop = requestAnimationFrame(zoomIn);
     return zoomInLoop;
   }
@@ -195,8 +174,7 @@ function init() {
     }
 
     positionPanner();
-    boomBox.style.webkitTransform = "translate(" + boomX + "px , " + boomY + "px) scale(" + boomZoom + ")";
-    boomBox.style.transform = "translate(" + boomX + "px , " + boomY + "px) scale(" + boomZoom + ")";
+    boomBox.style.transform = boomBox.style.transform = `translate(${boomX}px, ${boomY}px) scale(${boomZoom})`;
     zoomOutLoop = requestAnimationFrame(zoomOut);
     return zoomOutLoop;
   }
@@ -205,41 +183,40 @@ function init() {
   // onmouseup cancels the resulting requestAnimationFrames.
 
   leftButton.onmousedown = moveLeft;
-  leftButton.onmouseup = function () {
-    window.cancelAnimationFrame(leftLoop);
+  leftButton.onmouseup = () => {
+    cancelAnimationFrame(leftLoop);
   };
 
   rightButton.onmousedown = moveRight;
-  rightButton.onmouseup = function () {
-    window.cancelAnimationFrame(rightLoop);
+  rightButton.onmouseup = () => {
+    cancelAnimationFrame(rightLoop);
   };
 
   zoomInButton.onmousedown = zoomIn;
-  zoomInButton.onmouseup = function () {
+  zoomInButton.onmouseup = () => {
     window.cancelAnimationFrame(zoomInLoop);
   };
 
   zoomOutButton.onmousedown = zoomOut;
-  zoomOutButton.onmouseup = function () {
+  zoomOutButton.onmouseup = () => {
     window.cancelAnimationFrame(zoomOutLoop);
   };
 }
-// wire up buttons to stop and play audio
 
-stop.setAttribute('disabled', 'disabled');
+// Wire up buttons to stop and play audio
+stopBtn.disabled = true;
 
-play.onclick = function() {
+playBtn.onclick = () => {
   init();
-  source.start(0);
 
-  play.setAttribute('disabled', 'disabled');
-  stop.removeAttribute('disabled');
-  pulseWrapper.classList.add('pulsate');
+  playBtn.disabled = true;
+  stopBtn.disabled = false;
+  pulseWrapper.classList.add('pulsate')
 };
 
-stop.onclick = function() {
+stopBtn.onclick = () => {
   source.stop(0);
-  stop.setAttribute('disabled', 'disabled');
-  play.removeAttribute('disabled');
+  playBtn.disabled = false;
+  stopBtn.disabled = true;
   pulseWrapper.classList.remove('pulsate');
 };
