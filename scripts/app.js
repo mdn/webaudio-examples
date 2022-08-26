99120f6fab65aaeb56c3198a4d0594b8cfab49d3
DJ MoleHill <djmolehill@gmail.com>
2022-08-25 21:36:24 -0500

    Code style cleanup
    * Run clang-format on app.js
    * Prefer === to == when relevant
    * Consistently use single quotes (') vs mixing with double quotes (")
    * Consistently use semicolons
    * Prefer let and const to var

--- scripts/app.js
+++ scripts/app.js
@@ -1,164 +1,153 @@
-let heading = document.querySelector('h1');
+const heading = document.querySelector('h1');
 heading.textContent = 'CLICK ANYWHERE TO START'
 document.body.addEventListener('click', init);
 
-
 function init() {
   heading.textContent = 'Voice-change-O-matic';
-  document.body.removeEventListener('click', init)
+  document.body.removeEventListener('click', init);
 
-  // Older browsers might not implement mediaDevices at all, so we set an empty object first
+  // Older browsers might not implement mediaDevices at all, so we set an empty
+  // object first.
   if (navigator.mediaDevices === undefined) {
     navigator.mediaDevices = {};
   }
 
-
-  // Some browsers partially implement mediaDevices. We can't just assign an object
-  // with getUserMedia as it would overwrite existing properties.
-  // Here, we will just add the getUserMedia property if it's missing.
+  // Some browsers partially implement mediaDevices. We can't just assign an
+  // object with getUserMedia as it would overwrite existing properties. Here,
+  // we will just add the getUserMedia property if it's missing.
   if (navigator.mediaDevices.getUserMedia === undefined) {
     navigator.mediaDevices.getUserMedia = function(constraints) {
+      // First get ahold of the legacy getUserMedia, if present.
+      const getUserMedia = navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia;
 
-      // First get ahold of the legacy getUserMedia, if present
-      var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-      // Some browsers just don't implement it - return a rejected promise with an error
-      // to keep a consistent interface
+      // Some browsers just don't implement it - return a rejected promise with
+      // an error to keep a consistent interface
       if (!getUserMedia) {
-        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        return Promise.reject(
+            new Error('getUserMedia is not implemented in this browser'));
       }
 
-      // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+      // Otherwise, wrap the call to the old navigator.getUserMedia with a
+      // Promise.
       return new Promise(function(resolve, reject) {
         getUserMedia.call(navigator, constraints, resolve, reject);
       });
     }
   }
 
+  // Set up forked web audio context, for multiple browsers
+  // window. It is required for safari.
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  const voiceSelect = document.getElementById("voice");
+  let source;
+  let stream;
 
+  // Grab the mute button to use below.
+  const mute = document.querySelector('.mute');
 
-  // set up forked web audio context, for multiple browsers
-  // window. is needed otherwise Safari explodes
-
-  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  var voiceSelect = document.getElementById("voice");
-  var source;
-  var stream;
-
-  // grab the mute button to use below
-
-  var mute = document.querySelector('.mute');
-
-  //set up the different audio nodes we will use for the app
-
-  var analyser = audioCtx.createAnalyser();
+  // Set up the different audio nodes we will use for the app.
+  const analyser = audioCtx.createAnalyser();
   analyser.minDecibels = -90;
   analyser.maxDecibels = -10;
   analyser.smoothingTimeConstant = 0.85;
 
-  var distortion = audioCtx.createWaveShaper();
-  var gainNode = audioCtx.createGain();
-  var biquadFilter = audioCtx.createBiquadFilter();
-  var convolver = audioCtx.createConvolver();
+  const distortion = audioCtx.createWaveShaper();
+  const gainNode = audioCtx.createGain();
+  const biquadFilter = audioCtx.createBiquadFilter();
+  const convolver = audioCtx.createConvolver();
 
-  // distortion curve for the waveshaper, thanks to Kevin Ennis
+  // Distortion curve for the waveshaper, thanks to Kevin Ennis
   // http://stackoverflow.com/questions/22312841/waveshaper-node-in-webaudio-how-to-emulate-distortion
-
   function makeDistortionCurve(amount) {
-    var k = typeof amount === 'number' ? amount : 50,
-      n_samples = 44100,
-      curve = new Float32Array(n_samples),
-      deg = Math.PI / 180,
-      i = 0,
-      x;
-    for ( ; i < n_samples; ++i ) {
+    var k = typeof amount === 'number' ? amount : 50, n_samples = 44100,
+        curve = new Float32Array(n_samples), deg = Math.PI / 180, i = 0, x;
+    for (; i < n_samples; ++i) {
       x = i * 2 / n_samples - 1;
-      curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+      curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
     }
     return curve;
   };
 
-  // grab audio track via XHR for convolver node
-
-  var soundSource;
-
+  // Grab audio track via XHR for convolver node.
+  let soundSource;
   ajaxRequest = new XMLHttpRequest();
 
-  ajaxRequest.open('GET', 'https://mdn.github.io/voice-change-o-matic/audio/concert-crowd.ogg', true);
+  ajaxRequest.open(
+      'GET',
+      'https://mdn.github.io/voice-change-o-matic/audio/concert-crowd.ogg',
+      true);
 
   ajaxRequest.responseType = 'arraybuffer';
 
-
   ajaxRequest.onload = function() {
-    var audioData = ajaxRequest.response;
+    const audioData = ajaxRequest.response;
+    audioCtx.decodeAudioData(
+        audioData,
+        function(buffer) {
+          soundSource = audioCtx.createBufferSource();
+          convolver.buffer = buffer;
+        },
+        function(e) { console.log('Error with decoding audio data' + e.err); });
 
-    audioCtx.decodeAudioData(audioData, function(buffer) {
-        soundSource = audioCtx.createBufferSource();
-        convolver.buffer = buffer;
-      }, function(e){ console.log("Error with decoding audio data" + e.err);});
-
-    //soundSource.connect(audioCtx.destination);
-    //soundSource.loop = true;
-    //soundSource.start();
+    // soundSource.connect(audioCtx.destination);
+    // soundSource.loop = true;
+    // soundSource.start();
   };
 
   ajaxRequest.send();
 
-  // set up canvas context for visualizer
+  // Set up canvas context for visualizer.
+  const canvas = document.querySelector('.visualizer');
+  const canvasCtx = canvas.getContext('2d');
 
-  var canvas = document.querySelector('.visualizer');
-  var canvasCtx = canvas.getContext("2d");
+  const intendedWidth = document.querySelector('.wrapper').clientWidth;
 
-  var intendedWidth = document.querySelector('.wrapper').clientWidth;
+  canvas.setAttribute('width', intendedWidth);
 
-  canvas.setAttribute('width',intendedWidth);
+  const visualSelect = document.getElementById('visual');
+  let drawVisual;
 
-  var visualSelect = document.getElementById("visual");
-
-  var drawVisual;
-
-  //main block for doing the audio recording
-
+  // Main block for doing the audio recording:
   if (navigator.mediaDevices.getUserMedia) {
-     console.log('getUserMedia supported.');
-     var constraints = {audio: true}
-     navigator.mediaDevices.getUserMedia (constraints)
-        .then(
-          function(stream) {
-             source = audioCtx.createMediaStreamSource(stream);
-             source.connect(distortion);
-             distortion.connect(biquadFilter);
-             biquadFilter.connect(gainNode);
-             convolver.connect(gainNode);
-             gainNode.connect(analyser);
-             analyser.connect(audioCtx.destination);
+    console.log('getUserMedia supported.');
+    const constraints = {audio : true};
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(stream) {
+          source = audioCtx.createMediaStreamSource(stream);
+          source.connect(distortion);
+          distortion.connect(biquadFilter);
+          biquadFilter.connect(gainNode);
+          convolver.connect(gainNode);
+          gainNode.connect(analyser);
+          analyser.connect(audioCtx.destination);
 
-          	 visualize();
-             voiceChange();
+          visualize();
+          voiceChange();
         })
-        .catch( function(err) { console.log('The following gUM error occured: ' + err);})
+        .catch(function(
+            err) { console.log('The following gUM error occured: ' + err); })
   } else {
-     console.log('getUserMedia not supported on your browser!');
+    console.log('getUserMedia not supported on your browser!');
   }
 
   function visualize() {
     WIDTH = canvas.width;
     HEIGHT = canvas.height;
 
-
-    var visualSetting = visualSelect.value;
+    const visualSetting = visualSelect.value;
     console.log(visualSetting);
 
-    if(visualSetting === "sinewave") {
+    if (visualSetting === 'sinewave') {
       analyser.fftSize = 2048;
-      var bufferLength = analyser.fftSize;
+      const bufferLength = analyser.fftSize;
       console.log(bufferLength);
-      var dataArray = new Uint8Array(bufferLength);
+      const dataArray = new Uint8Array(bufferLength);
 
       canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-      var draw = function() {
-
+      const draw = function() {
         drawVisual = requestAnimationFrame(draw);
 
         analyser.getByteTimeDomainData(dataArray);
@@ -171,15 +160,14 @@ function init() {
 
         canvasCtx.beginPath();
 
-        var sliceWidth = WIDTH * 1.0 / bufferLength;
-        var x = 0;
+        const sliceWidth = WIDTH * 1.0 / bufferLength;
+        let x = 0;
 
-        for(var i = 0; i < bufferLength; i++) {
-
+        for (let i = 0; i < bufferLength; i++) {
           var v = dataArray[i] / 128.0;
-          var y = v * HEIGHT/2;
+          var y = v * HEIGHT / 2;
 
-          if(i === 0) {
+          if (i === 0) {
             canvasCtx.moveTo(x, y);
           } else {
             canvasCtx.lineTo(x, y);
@@ -188,21 +176,21 @@ function init() {
           x += sliceWidth;
         }
 
-        canvasCtx.lineTo(canvas.width, canvas.height/2);
+        canvasCtx.lineTo(canvas.width, canvas.height / 2);
         canvasCtx.stroke();
       };
 
       draw();
 
-    } else if(visualSetting == "frequencybars") {
+    } else if (visualSetting == 'frequencybars') {
       analyser.fftSize = 256;
-      var bufferLengthAlt = analyser.frequencyBinCount;
+      const bufferLengthAlt = analyser.frequencyBinCount;
       console.log(bufferLengthAlt);
-      var dataArrayAlt = new Uint8Array(bufferLengthAlt);
+      const dataArrayAlt = new Uint8Array(bufferLengthAlt);
 
       canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-      var drawAlt = function() {
+      const drawAlt = function() {
         drawVisual = requestAnimationFrame(drawAlt);
 
         analyser.getByteFrequencyData(dataArrayAlt);
@@ -210,15 +198,16 @@ function init() {
         canvasCtx.fillStyle = 'rgb(0, 0, 0)';
         canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-        var barWidth = (WIDTH / bufferLengthAlt) * 2.5;
-        var barHeight;
-        var x = 0;
+        const barWidth = (WIDTH / bufferLengthAlt) * 2.5;
+        let barHeight;
+        let x = 0;
 
-        for(var i = 0; i < bufferLengthAlt; i++) {
+        for (let i = 0; i < bufferLengthAlt; i++) {
           barHeight = dataArrayAlt[i];
 
-          canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
-          canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
+          canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+          canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth,
+                             barHeight / 2);
 
           x += barWidth + 1;
         }
@@ -226,64 +215,59 @@ function init() {
 
       drawAlt();
 
-    } else if(visualSetting == "off") {
+    } else if (visualSetting == 'off') {
       canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-      canvasCtx.fillStyle = "red";
+      canvasCtx.fillStyle = 'red';
       canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
     }
-
   }
 
   function voiceChange() {
-
     distortion.oversample = '4x';
     biquadFilter.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
 
-    var voiceSetting = voiceSelect.value;
+    const voiceSetting = voiceSelect.value;
     console.log(voiceSetting);
 
-    //when convolver is selected it is connected back into the audio path
-    if(voiceSetting == "convolver") {
+    // When convolver is selected it is connected back into the audio path.
+    if (voiceSetting == 'convolver') {
       biquadFilter.disconnect(0);
       biquadFilter.connect(convolver);
     } else {
       biquadFilter.disconnect(0);
       biquadFilter.connect(gainNode);
 
-      if(voiceSetting == "distortion") {
+      if (voiceSetting === 'distortion') {
         distortion.curve = makeDistortionCurve(400);
-      } else if(voiceSetting == "biquad") {
-        biquadFilter.type = "lowshelf";
+      } else if (voiceSetting === 'biquad') {
+        biquadFilter.type = 'lowshelf';
         biquadFilter.frequency.setTargetAtTime(1000, audioCtx.currentTime, 0)
         biquadFilter.gain.setTargetAtTime(25, audioCtx.currentTime, 0)
-      } else if(voiceSetting == "off") {
-        console.log("Voice settings turned off");
+      } else if (voiceSetting === 'off') {
+        console.log('Voice settings turned off');
       }
     }
   }
 
-  // event listeners to change visualize and voice settings
-
+  // Event listeners to change visualize and voice settings:
   visualSelect.onchange = function() {
     window.cancelAnimationFrame(drawVisual);
     visualize();
   };
 
-  voiceSelect.onchange = function() {
-    voiceChange();
-  };
+  voiceSelect.onchange = function() { voiceChange(); };
 
   mute.onclick = voiceMute;
 
   function voiceMute() {
-    if(mute.id === "") {
+    if (mute.id === '') {
       gainNode.gain.value = 0;
-      mute.id = "activated";
-      mute.innerHTML = "Unmute";
+      mute.id = 'activated';
+      mute.innerHTML = 'Unmute';
     } else {
       gainNode.gain.value = 1;
-      mute.id = "";
-      mute.innerHTML = "Mute";
+      mute.id = '';
+      mute.innerHTML = 'Mute';
     }
   }
 }
